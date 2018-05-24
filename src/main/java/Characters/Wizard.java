d5c60c61f8d11f710faca9ebe02f9480d7de2ce5
1anchen <svgyc@hotmail.co.uk>
2018-05-24 13:26:02 +0100

    start the wizard class

--- src/main/java/Characters/Wizard.java
+++ src/main/java/Characters/Wizard.java
@@ -1,4 +1,28 @@
 package Characters;
+import Interface.IWeapon;
 
-public class Wizard {
+
+public class Wizard  extends Characters implements IWeapon {
+
+    protected String spell;
+    protected String pet;
+
+    public Wizard(String name, String weapon, int health, String spell, String pet) {
+        super(name, weapon, health);
+        this.spell = spell;
+        this.pet = pet;
+    }
+
+    public String drawWeapon() {
+        String newWeapon = weapons.get(0);
+        return newWeapon;
+    }
+
+    public String getSpell() {
+        return this.spell;
+    }
+
+    public String getPet() {
+        return this.pet;
+    }
 }
