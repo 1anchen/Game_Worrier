13587175a74ac9b3e4f37ba00cf8903f29c1d403
1anchen <svgyc@hotmail.co.uk>
2018-05-24 13:38:20 +0100

    started interface Ispellandpet

--- src/test/java/WizardTest.java
+++ src/test/java/WizardTest.java
@@ -1,2 +1,54 @@
+import Characters.Wizard;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WizardTest {
+
+    Wizard wizard;
+
+    @Before
+    public void before() {
+        this.wizard = new Wizard("Gandalf", "Staff", 125,"Lighting Strike","Dragon");
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Gandalf", wizard.getName());
+    }
+
+    @Test
+    public void canGetWeapon() {
+        assertEquals("Staff", wizard.getWeapon());
+    }
+
+    @Test
+    public void canGetHealth() {
+        assertEquals(125, wizard.getHealth());
+    }
+
+    @Test
+    public void canGetSpell(){
+        assertEquals("Lighting Strike",wizard.getSpell());
+    }
+
+    @Test
+    public void canGetPet(){
+        assertEquals("Dragon",wizard.getPet());
+    }
+
+    @Test
+    public void canAddWeapon() {
+        assertEquals(0, wizard.weaponCount());
+        wizard.addWeapon("Sword");
+        assertEquals(1, wizard.weaponCount());
+    }
+
+    @Test
+    public void canSwitchWeapon() {
+        wizard.addWeapon("Sword");
+        wizard.drawWeapon();
+        assertEquals("Sword", wizard.drawWeapon());
+    }
 }
