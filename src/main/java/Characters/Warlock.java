555992935f03323acf9421008e73c6ba4f9439f1
1anchen <svgyc@hotmail.co.uk>
2018-05-24 13:16:08 +0100

    strated the warlocktest class

--- src/main/java/Characters/Warlock.java
+++ src/main/java/Characters/Warlock.java
@@ -1,4 +1,22 @@
 package Characters;
+import Interface.IWeapon;
 
-public class Warlock {
+
+public class Warlock  extends Characters implements IWeapon {
+
+    protected String spell;
+    protected String pet;
+
+    public Warlock(String name, String weapon, int health, String spell, String pet) {
+        super(name, weapon, health);
+        this.spell = spell;
+        this.pet = pet;
+
+    }
+
+    public String drawWeapon() {
+        String newWeapon = weapons.get(0);
+        return newWeapon;
+    }
+
 }
