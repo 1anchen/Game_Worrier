a4d6673e1b04f6aae9778fc2d7fbd66acac8cbd7
1anchen <svgyc@hotmail.co.uk>
2018-05-24 14:24:54 +0100

    cleric test started

--- src/main/java/Room/Cleric.java
+++ src/main/java/Room/Cleric.java
@@ -1,4 +1,18 @@
 package Room;
 
+import java.util.ArrayList;
+
 public class Cleric {
+    protected ArrayList<String> potions;
+
+    public Cleric(){
+        this.potions = new ArrayList<>();
+        this.potions.add("Healing Potion");
+        this.potions.add("Healing Hearb");
+        this.potions.add("Healing Gel");
+    }
+
+    public String getPotion(){
+        return this.potions.get(0);
+    }
 }
